b5176ada8b85ae94a2b92396996351d451fe1838
kimjouny <juny4500@naver.com>
2020-08-27 17:25:47 +0900

    [FEAT] config index page

--- public/javascripts/main.mjs
+++ public/javascripts/main.mjs
@@ -1,8 +1,17 @@
+
+
 let xDown = null;                                                        
 let yDown = null;
 
 const SCROLL_VIEW=document.getElementsByClassName('slide_container')[0];
+const OVERLAY=document.getElementsByClassName('overlay_section')[0];
+window.onload=()=>{
+    setTimeout(()=>{OVERLAY.style.opacity='0'},1500);
+}
 
+OVERLAY.addEventListener('transitionend',()=>{
+    OVERLAY.style.display='none';
+})
 
 const getTouches=(evt)=>{return evt.touches;}                                                     
   
@@ -31,3 +40,6 @@ const handleTouchMove=(evt)=>{
 
 SCROLL_VIEW.addEventListener('touchstart',handleTouchStart);
 SCROLL_VIEW.addEventListener('touchmove',handleTouchMove)
+
+const PROTOTYPES=document.getElementsByClassName('version_el');
+PROTOTYPES[0].addEventListener('click',()=>window.location.href="/v1.0")
--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,12 @@ router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
-router.get('/decumulation', function(req, res, next) {
+router.get('/v1.0', (req, res, next)=> {
   // res.render('index', { title: 'Express' });
   res.render('graph.html');
 });
+
+router.get('/v1.1', (req,res,next)=>{
+  res.render('carousel.html');
+})
 module.exports = router;
