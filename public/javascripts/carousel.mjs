2b4ca8e4aa2d349cd37fd81d70e014654764d895
kimjouny <juny4500@naver.com>
2020-08-31 14:23:32 +0900

    [REFACTOR] divide carousel component script to carousel.mjs

--- public/javascripts/carousel.mjs
+++ public/javascripts/carousel.mjs
@@ -1,5 +1,46 @@
-CAROUSEL_BTNS=document.getElementsByClassName('carousel_btn_wrapper');
 
-CAROUSEL_BTNS[0].addEventListener('click',(e)=>{
-    console.log(e.target)
-});
+let xDown = [null,null];                                                        
+let xPos = [0,0];
+let carouselRun=false;
+const getTouches=(evt)=> {return evt.touches; }             // browser API}                                                     
+
+const handleTouchStart=(evt,idx)=>{
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown[idx] = firstTouch.clientX;                                                                      
+};                                                
+
+const handleTouchMove=(evt,idx)=> {
+    if (!xDown || carouselRun )return;
+    var xUp = evt.touches[0].clientX;                                    
+    var xDiff = xDown[idx] - xUp;
+    if ( xDiff > 0 ) {
+        /* left swipe */ 
+        if(xPos[idx]>=2)return;
+        evt.currentTarget.style.transform=`translateX(${(++xPos[idx])*(-100)}%)`
+        carouselRun=true;
+    } else {
+        /* right swipe */
+        if(xPos[idx]<1)return;
+        evt.currentTarget.style.transform=`translateX(${(--xPos[idx])*(-100)}%)`
+        carouselRun=true;
+    }                       
+    /* reset values */
+    xDown[idx] = null;                                             
+};
+
+const handleCarouselBtn=(e)=>{
+    if(e.target.tagName!=="LI")return;
+    CAROUSEL_WRAP[i].style.transform=`translateX(${e.target.value*(-100)}%)`
+}
+
+
+const initCarousel=(CAROUSEL_WRAP,CAROUSEL_BTNS)=>{
+    for(let i=0;i<CAROUSEL_WRAP.length;i++){
+        CAROUSEL_WRAP[i].addEventListener('touchstart',(e)=>handleTouchStart(e,i),false);
+        CAROUSEL_WRAP[i].addEventListener('touchmove',(e)=>handleTouchMove(e,i),false);
+        CAROUSEL_BTNS[i].addEventListener('click',handleCarouselBtn)
+        CAROUSEL_WRAP[i].addEventListener('transitionend',()=>carouselRun=false);
+    }
+}
+
+export {initCarousel}
--- public/javascripts/index_v1_1.mjs
+++ public/javascripts/index_v1_1.mjs
@@ -1,43 +1,6 @@
+import {initCarousel} from './carousel.mjs'
+
 const CAROUSEL_BTNS=document.getElementsByClassName('carousel_btn_wrapper');
 const CAROUSEL_WRAP=document.getElementsByClassName('carousel_wrapper');
 
-
-let xDown = [null,null];                                                        
-let xPos = [0,0];
-let carouselRun=false;
-const getTouches=(evt)=> {return evt.touches; }             // browser API}                                                     
-
-const handleTouchStart=(evt,idx)=>{
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown[idx] = firstTouch.clientX;                                                                      
-};                                                
-
-const handleTouchMove=(evt,idx)=> {
-    if (!xDown || carouselRun )return;
-    var xUp = evt.touches[0].clientX;                                    
-    var xDiff = xDown[idx] - xUp;
-    if ( xDiff > 0 ) {
-        /* left swipe */ 
-        if(xPos[idx]>=2)return;
-        CAROUSEL_WRAP[idx].style.transform=`translateX(${(++xPos[idx])*(-100)}%)`
-        carouselRun=true;
-    } else {
-        /* right swipe */
-        if(xPos[idx]<1)return;
-        CAROUSEL_WRAP[idx].style.transform=`translateX(${(--xPos[idx])*(-100)}%)`
-        carouselRun=true;
-    }                       
-    /* reset values */
-    xDown[idx] = null;                                             
-};
-
-for(let i=0;i<CAROUSEL_WRAP.length;i++){
-    CAROUSEL_WRAP[i].addEventListener('touchstart',(e)=>handleTouchStart(e,i),false);
-    CAROUSEL_WRAP[i].addEventListener('touchmove',(e)=>handleTouchMove(e,i),false);
-    CAROUSEL_BTNS[i].addEventListener('click',(e)=>{
-        if(e.target.tagName!=="LI")return;
-        CAROUSEL_WRAP[i].style.transform=`translateX(${e.target.value*(-100)}%)`
-    })
-    CAROUSEL_WRAP[i].addEventListener('transitionend',()=>carouselRun=false);
-}
-
+initCarousel(CAROUSEL_WRAP,CAROUSEL_BTNS);
